4709690028ffb964d5f68afcd71d17530bf45175
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-22 00:20:56 +0100

    feat: Configure ESLint and TypeScript settings in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has TypeScript errors.
+		ignoreBuildErrors: true,
+	},
+	images: {
+		remotePatterns: [{ hostname: "cdn.sanity.io" }],
+	},
 };
 
 export default nextConfig;
