c351dedef0c13d0885744cd1893ffa9bc7ef826a
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-27 00:15:01 +0100

    --

---
+++ utils/projects.ts
@@ -0,0 +1,86 @@
+export const websiteProjects = [
+	{
+		title: "Imperia Consulting, Nairobi, Kenya",
+		url: "",
+		image: "",
+	},
+	{
+		title: "Vedura Resort, Lagos, Nigeria",
+		url: "",
+		image: "",
+	},
+	{
+		title: "Awo Tech Mall, Lagos, Nigeria",
+		url: "",
+		image: "",
+	},
+	{
+		title: "BTech 360, Lagos, Nigeria",
+		url: "",
+		image: "",
+	},
+	{
+		title: "Explore 360, Lagos, Nigeria",
+		url: "",
+		image: "",
+	},
+];
+
+export const webApplications = [
+	{
+		title: "",
+		stack: [
+			{
+				title: "",
+				icon: "",
+			},
+		],
+		url: "",
+		description: "",
+	},
+
+	{
+		title: "",
+		stack: [
+			{
+				title: "",
+				icon: "",
+			},
+		],
+		url: "",
+		description: "",
+	},
+
+	{
+		title: "",
+		stack: [
+			{
+				title: "",
+				icon: "",
+			},
+		],
+		url: "",
+		description: "",
+	},
+];
+
+export const apiService = [
+	{
+		title: "",
+		description: "",
+		repo: "",
+		docs: "",
+	},
+	{
+		title: "",
+		description: "",
+		repo: "",
+		docs: "",
+	},
+	{
+		title: "",
+		description: "",
+		repo: "",
+		docs: "",
+	},
+];
